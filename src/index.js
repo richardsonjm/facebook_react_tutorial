fe7f9874627e34b17e642292601bb760e57579f4
richardsonjm <richardsonjm@gmail.com>
2017-02-18 16:53:26 -0500

    Take turns between X and O

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ class Board extends React.Component {
     super();
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
   renderSquare(i) {
@@ -22,11 +23,14 @@ class Board extends React.Component {
   }
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     return (
       <div>
         <div className="status">{status}</div>
